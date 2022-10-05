2b4e46a072d32b12e848fd7ade1fcb0bd0420f5b
Thomas Torku <thomas.torku@mtsu.edu>
2022-10-05 15:07:51 -0500

    updated files

---
+++ script1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from mpi4py import MPI
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from time import process_time    #checking cpu time
+from sklearn.preprocessing import MinMaxScaler 
+import time
+
+comm = MPI.COMM_WORLD
+rank = comm.rank
+size = comm.size
+name = MPI.Get_processor_name()
+if rank == 0:
+    #start = process_time()
+    data_process(data, scaler, "no")  #unscaled
+    print("rank0 finished")
+#get the start time
